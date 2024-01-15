af8f51c890a7d3d4fe5864fee5bbfde7802775a1
Akash kumar <80808362+akashkumar220011@users.noreply.github.com>
2024-01-16 01:43:17 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const mongoDB = mongoose.connect('mongodb://localhost:27017/IMS')
+const mongoDB = mongoose.connect('mongodb+srv://krakash1022:aksh09876@cluster0.0n57yxv.mongodb.net/IMS')
 
 module.exports = mongoDB
